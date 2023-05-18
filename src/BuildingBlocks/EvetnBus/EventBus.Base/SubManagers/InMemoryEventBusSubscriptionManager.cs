4900360415c85eff8c879086c5afdc0d2d382707
Sultan Özgür <88083356+sultan-zgr@users.noreply.github.com>
2023-05-18 17:46:55 +0300

    Add project files.

---
+++ src/BuildingBlocks/EvetnBus/EventBus.Base/SubManagers/InMemoryEventBusSubscriptionManager.cs
@@ -0,0 +1,53 @@
+﻿using EventBus.Base.Abstraction;
+using EventBus.Base.Events;
+using Newtonsoft.Json.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.SubManagers
+{
+    public class InMemoryEventBusSubscriptionManager : IEventBusSubscriptionManager
+    {
+        private readonly Dictionary<string, List<SubscriptionInfo>> _handlers;
+        private readonly List<Type> _eventTypes;
+
+        public event EventHandler<string> OnEventRemoved;
+        public Func<string, string> eventNameGetter;
+
+        public InMemoryEventBusSubscriptionManager(Func<string, string> eventNameGetter)
+        {
+            _handlers = new Dictionary<string, List<SubscriptionInfo>>();
+            _eventTypes = new List<Type>();
+            this.eventNameGetter = eventNameGetter;
+        }
+        public bool IsEmpty => !_handlers.Keys.Any();
+        public void Clear() => _handlers.Clear();
+
+        public void AddSubscription<T, TH>() where T: IntegrationEvent where TH : IIntegrationEventHandler<T> 
+        {
+            var eventName = GetEventKey<T>();
+            
+            AddSubscription(typeof(TH), eventName);
+            if (!_eventTypes.Contains(typeof(T)))
+            {
+                _eventTypes.Add(typeof(T));
+            }
+
+
+        }
+
+        private void AddSubscription(Type type, object eventName)
+        {
+            throw new NotImplementedException();
+        }
+
+        private object GetEventKey<T>() where T : IntegrationEvent
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
